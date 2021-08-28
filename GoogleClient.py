34690b46f763161eac10119006d38eb1915cc0e3
Michael Barry <micahel23barry@gmail.com>
2021-08-27 21:21:57 -0400

    updating NotionPyApi.py to save progress

--- GoogleClient.py
+++ GoogleClient.py
@@ -5,26 +5,59 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-def GoogleClient():
-    #quickstart from goog api - begin
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json')
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+class GoogleClient():
+    def __init__(self):
+        self.service = None
 
-    service = build('tasks', 'v1', credentials=creds)
-    return service
-    
+    def set_up_service():
+        #quickstart from goog api - begin
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json')
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        self.service = build('tasks', 'v1', credentials=creds)
+
+class TaskLists():
+    def __init__(self, results_json):
+        self.kind = results_json['kind']
+        self.etag = results_json['etag']
+        self.nextPageToken = results_json['nextPageToken']
+        self.items = results_json['items'] # this is a list of all of the tasks
+
+class Tasks():
+    def __init__(self, items):
+        self.kind = results_json['kind']
+        self.id = results_json['id']
+        self.etag = results_json['etag']
+        self.title = results_json['title']
+        self.updated = results_json['updated']
+        self.selfLink = results_json['selfLink']
+        self.position = results_json['position']
+        self.notes = results_json['notes']
+        self.status = results_json['status']
+        self.due = results_json['due']
+
+"""Add function to update (patch) a Task"""
+"""Add function to delete (archieve) a Task"""
+
+    # Call the Tasks API
+    results = service.tasklists().list(maxResults=10).execute()
+    items = results.get('items', [])
+    print(items)
+    task_list = service.tasks().list(tasklist='MDgyMDQzMTQxMzIyMTgyMDcxMzI6MDow', maxResults=10).execute()
+    print("task list")
+    print(task_list)
--- NotionPyApi.py
+++ NotionPyApi.py
@@ -5,12 +5,12 @@ import requests
 
 class Database():
     """Parent Node Database"""
-    def __init__(self, database_id, notion_url, integration_token):
+    def __init__(self, database_id, notion_db_url, integration_token):
         # variables for syncing the database
         self.database_id = database_id
-        self.notion_url = notion_url
+        self.notion_db_url = notion_db_url
         self.integration_token = integration_token
-        self.database_url = self.notion_url + self.database_id
+        self.cmplt_database_url = self.notion_db_url + self.database_id
         #self.data_json = self.query_database(database_url, integration_token)
         self.database = {}
         self.all_task_ids_dict = {}
@@ -25,7 +25,7 @@ class Database():
 
     def query_database(self):
         #queries the entire database and returns it as a json file
-        temp_url = self.database_url+"/query"
+        temp_url = self.cmplt_database_url+"/query"
         # need to use post keyword when querying for some reason -> review request documentation
         response = requests.post(temp_url, headers={"Authorization": f"{self.integration_token}", "Notion-Version": "2021-08-16" })
         self.data_json = response.json()
@@ -44,44 +44,47 @@ class Page(Database):
     """Child Node of Parent"""
     def __init__(self, page_data_json):
         self.page = {}
+        self.notion_pg_url = "https://api.notion.com/v1/pages/"
         #function calls to create variables?
         #self.page[self.pObject]: page_data_json['object']
         self.pObject = page_data_json['object']
         self.id = page_data_json['id']
         self.created_time = page_data_json['created_time']
         self.last_edited_time = page_data_json['last_edited_time']
-        self.parent = page_data_json['parent']
+        self.cover = page_data_json['cover']
+        self.parent = page_data_json['parent']['database_id']
         self.archived = page_data_json['archived']
         self.properties = page_data_json['properties']
-        self.Links = page_data_json['Links']
-        self.Due_Date = page_data_json['Due_Date']
-        self.Google_Task_ID = page_data_json['Google_Task_ID']
-        self.Status  = page_data_json['Status']
-        self.Task_Name = page_data_json['Task_Name']
-
         Database.__init__(self)
 
-
-
+    def update_page(self, Due_Date, Google_Task_ID, Task_Name, Notes):
+        #queries the entire database and returns it as a json file
+        temp_url = self.notion_pg_url+self.id
+        # need to use post keyword when querying for some reason -> review request documentation
+        response = requests.post(temp_url, headers={"Authorization": f"{self.integration_token}",
+        "Notion-Version": "2021-08-16"}, data = {'properties': {'Due Date': {'date' : {'start' : Due_Date}},
+        'Google Task ID': {'rich_text' : Google_Task_ID}, 'Task Name': {'title' : ['text': {'content': Task_Name},
+        'plain_text': Task_Name}]}, 'Notes': {'rich_text': Notes} })
         # need a too string method
 
+"""Add function to delete (archieve) a page"""
 
-class Block(Page):
-    def __init__(self, block_data_json, id):
-        self.block = {}
+class Properties(Page):
+    def __init__(self, properties_data_json, id):
+        self.properties_dict = {}
         #self.block[self.Overdue]: block_data_json['Overdue']
         #self.Overdue = block_data_json['Overdue']
-        self.Task_ID = id
         #self.Project_Tags = block_data_json['Project_Tags']
         #self.Links = block_data_json['Links']
-        self.Due_Date = block_data_json['Due Date']
+        self.Notes = properties_data_json['Notes']['rich_text']
+        self.Due_Date = properties_data_json['Due Date']['date']['start']
         #self.Related_to_Project_Table = block_data_json['Related_to_Project_Table']
-        self.Google_Task_ID = block_data_json['Google Task ID']
-        self.Status  = block_data_json['Status']
+        self.Google_Task_ID = properties_data_json['Google Task ID']['rich_text']
+        self.Status  = properties_data_json['Status']['checkbox']
         #self.Hi_Priority = block_data_json['Hi_Priority']
         #self.Related_to_Job_Applications = block_data_json['Related_to_Job_Applications']
         #self.Duration_in_Minutes = block_data_json['Duration_in_Minutes']
         #self.People_Linked = block_data_json['Duration_in_Minutes']
-        self.Task_Name = block_data_json['Task Name']
-
+        self.Task_Name = properties_data_json['Task Name']['title']['text']['plain_text']
+        Page.__init__(self, id, last_edited_time, archived)
         #need a too string method
